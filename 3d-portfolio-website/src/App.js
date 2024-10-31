550fa34a97398c3fbe725723ee7a2c10d37fa267
Chaitanya <chaitanyaasole@gmail.com>
2024-10-31 16:51:37 +0530

    started with navbar

--- 3d-portfolio-website/src/App.js
+++ 3d-portfolio-website/src/App.js
@@ -1,5 +1,13 @@
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Navbar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
