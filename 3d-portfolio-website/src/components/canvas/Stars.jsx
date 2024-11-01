79026df99ed10cf7dfaf819ee3579ba62eff6896
Chaitanya <chaitanyaasole@gmail.com>
2024-11-01 13:41:15 +0530

    Made funtion of stars

--- 3d-portfolio-website/src/components/canvas/Stars.jsx
+++ 3d-portfolio-website/src/components/canvas/Stars.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/radom/dist/maath-random.esm";
 import styled from "styled-components";
 
-const Stars = () => {
-  return <div>Stars</div>;
+const Stars = (props) => {
+  const ref = useRef();
+
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000)), {
+    radius: 1.2,
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        position={sphere}
+        stride={3}
+        frustumCulled
+        {...props}
+      ></Points>
+    </group>
+  );
 };
 
 export default Stars;
