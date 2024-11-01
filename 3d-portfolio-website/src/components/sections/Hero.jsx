474fca0f0c25db58206ed5db39c0d761535f23b9
Chaitanya <chaitanyaasole@gmail.com>
2024-11-01 13:12:46 +0530

    Added HeeroBgAnimation

--- 3d-portfolio-website/src/components/sections/Hero.jsx
+++ 3d-portfolio-website/src/components/sections/Hero.jsx
@@ -24,7 +24,7 @@ const HeroContainer = styled.div`
 `;
 
 const HeroInnerContainer = styled.div`
-  background-color: red;
+  // background-color: red;
   position: relative;
   display: flex;
   justify-content: space-between;
@@ -38,7 +38,7 @@ const HeroInnerContainer = styled.div`
 `;
 
 const HeroLeftContainer = styled.div`
-  background-color: blue;
+  // background-color: blue;
   width: 100%;
   order: 1;
 
@@ -53,10 +53,11 @@ const HeroLeftContainer = styled.div`
 `;
 
 const HeroRightContainer = styled.div`
-  background-color: orange;
+  // background-color: orange;
   width: 100%;
   order: 2;
-
+  display: flex;
+  justify-content: end;
   @media (max-width: 960px) {
     order: 1;
     display: flex;
@@ -193,7 +194,30 @@ const Img = styled.img`
   }
 `;
 
-const HeroBg = styled.div``;
+const HeroBg = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: end;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 1360px;
+  overflow: hidden;
+  padding: 0 30px;
+
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+
+  @media (max-width: 960px) {
+    justify-content: center;
+    padding: 0 0px;
+  }
+`;
 
 const Hero = () => {
   return (
