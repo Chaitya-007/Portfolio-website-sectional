02c039044f25b57f2529172d80252dc9a87b2715
Chaitanya <chaitanyaasole@gmail.com>
2024-11-01 15:53:45 +0530

    Designing The Skill Title

--- 3d-portfolio-website/src/components/sections/Skills.jsx
+++ 3d-portfolio-website/src/components/sections/Skills.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { skills } from "../../data/constants";
 
 const Container = styled.div`
   background-color: red;
@@ -39,17 +40,106 @@ const Title = styled.div`
   }
 `;
 
-const Desc = styled.div``;
+const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
+  color: ${({ theme }) => theme.text_secondary};
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const SkillsContainer = styled.div`
+  background: red;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 20px;
+  gap: 50px;
+  justify-content: center;
+`;
+
+const Skill = styled.div`
+  background: green;
+  width: 100%;
+  max-width: 500px;
+  background-color: rgba(17, 25, 40, 0.83);
+  border: 1px solid rgba(255, 255, 255, 0.125);
+  box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+  border-radius: 16px;
+  padding: 18px 36px;
+  @media (max-width: 768px) {
+    max-width: 400px;
+    padding: 10px 36px;
+  }
+
+  @media (max-width: 500px) {
+    max-width: 330px;
+    padding: 10px 36px;
+  }
+`;
+
+const SkillTitle = styled.div`
+  font-size: 28px;
+  font-weight: 600;
+  margin-bottom: 20px;
+  text-align: center;
+  color: ${({ theme }) => theme.text_secondary};
+`;
+
+const SkillList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+const SkillItem = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_primary + 80};
+  border: 1px solid ${({ theme }) => theme.text_secondary + 80};
+  border-radius: 12px;
+  padding: 12px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+`;
+
+const SkillImage = styled.img`
+  width: 24px;
+  height: 24px;
+`;
 
 const Skills = () => {
   return (
     <Container id="Skills">
       <Wrapper>
-        <Title>
-          <Desc>
-            Here are some of my skills on which I have bee working past 3 years.
-          </Desc>
-        </Title>
+        <Title>Skills</Title>
+        <Desc>
+          Here are some of my skills on which I have bee working past 3 years.
+        </Desc>
+
+        <SkillsContainer>
+          {skills.map((skill, index) => (
+            <Skill>
+              <SkillTitle>{skill.title}</SkillTitle>
+              <SkillList>
+                {skill.skills.map((item, index_x) => (
+                  <SkillItem>
+                    <SkillImage src={item.image} />
+                    {item.name}
+                  </SkillItem>
+                ))}
+              </SkillList>
+            </Skill>
+          ))}
+        </SkillsContainer>
       </Wrapper>
     </Container>
   );
